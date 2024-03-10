64754ab01ff8f29c80c8737a0df99989992858d7
Rahul keshri <rkeshri2021@gmail.com>
2024-03-10 12:56:44 +0530

    Mock Data

---
+++ src/constant/data.ts
@@ -0,0 +1,22 @@
+export const MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export const ORDERS_YEAR_ONE = [
+  100, 120, 150, 130, 180, 200, 220, 250, 280, 300, 200, 150,
+];
+
+export const ORDERS_YEAR_TWO = [
+  120, 110, 130, 140, 190, 210, 240, 260, 290, 310, 220, 180,
+];
